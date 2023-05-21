4600781330479345bae251cf457ce3c256a00c70
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2023-05-21 20:42:44 -0300

    add auth routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = Fastify()
-app.register(cors, { origin: true })
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
